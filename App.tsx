4f09917c4baa101052b8a93e4ceffb47757f1f44
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-01-26 23:42:16 +0500

    upadted

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 
+//Form Validation
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be minimum 4')
+    .max(16, 'Should no more longer than 16')
+    .required('This is required'),
+});
 const App = () => {
+  const [password, setPassword] = useState('');
+
   return (
     <View>
       <Text>App</Text>
